c8de5940b4aabf5760fab59d45d181658cbd57d3
PapouMarc <gamermarc090@gmail.com>
2022-11-17 19:53:34 +0100

    evol extension
    add scan extension
    choice type to scan
    only output for bad return

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,18 +8,32 @@ awk -F"[,:}]" '{for(i=1;i<=NF;i++){if($i~/'$KEY'\042/){print $(i+1)}}}' | tr -d 
 }
 
 function trapapi() {
+#only file '.css', '.html', '.htm' and '.svg'
+_ext="${1##*.}"
+_format=""
+case ${_ext} in 
+  "html"|"htm") _format="text/html"
+            ;;
+  "css") _format="text/css"
+       ;;
+  "svg") _format="image/svg+xml"
+       ;;
+esac
 
-curl -H "Content-Type: text/html; charset=utf-8" --data-binary @${1} https://validator.w3.org/nu/?out=json -o $$.json > /dev/null 2>&1
-_result=`cat $$.json | jsonValue message | sed 's/$/ /'`
-echo "########### [START] ${1} ###########\n${_result}\n########### [END] ${1} ###########\n"
-
+if [ "x${_format}" != "x" ]; then
+  curl -H "Content-Type: ${_format}; charset=utf-8" --data-binary @${1} https://validator.w3.org/nu/?out=json -o $$.json > /dev/null 2>&1
+  _result=`cat $$.json | jsonValue message | sed 's/$/ /'`
+  if [ "x${_result}" != "x" ]; then
+    echo "########### [START] ${1} ###########\n${_result}\n########### [END] ${1} ###########\n\n"
+  fi
+fi
 }
 
 
 if [ -e "${1}" ]; then
   if [ -d "${1}" ]; then
     for n in `ls -p1 ${1} | grep -v /`; do
-      result="${result}\n"`trapapi ${1}/$n`
+      result="${result}"`trapapi ${1}/$n`
     done
   else
     result=`trapapi ${1}`
